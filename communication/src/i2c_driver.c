574b08f3d48fbd86e5b75ef487ef346dadde6812
serkan <serkanmazlum306@gmail.com>
2024-01-07 22:29:54 +0300

    paket konumları değiştirildi

--- communication/src/i2c_driver.c
+++ communication/src/i2c_driver.c
@@ -4,35 +4,34 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <i2c/smbus.h>
 #include <linux/i2c-dev.h>
 
 #include "i2c_driver.h"
 
-static int fd;
-
-int i2c_init(const char *device, uint8_t address) {
-    fd = open(device, O_RDWR);
-    if (fd < 0) {
+int i2c_init(int *fd, const char *device, uint8_t address) {
+    *fd = open(device, O_RDWR);
+    if (*fd < 0) {
         perror("Could not open the I2C device");
         return -1;
     }
 
-    if (ioctl(fd, I2C_SLAVE, address) < 0) {
+    if (ioctl(*fd, I2C_SLAVE, address) < 0) {
         perror("Could not set I2C device address");
-        close(fd);
+        close(*fd);
         return -1;
     }
 
     return 0;
 }
 
-int i2c_read_register(int fd, uint8_t reg, uint8_t *data, size_t len) {
-    if (write(fd, &reg, 1) != 1) {
+int i2c_read_register(int *fd, uint8_t reg, uint8_t *data, size_t len) {
+    if (write(*fd, &reg, 1) != 1) {
         perror("Failed to write register address");
         return -1;
     }
 
-    if (read(fd, data, len) != len) {
+    if (read(*fd, data, len) != len) {
         perror("Failed to read from register");
         return -1;
     }
@@ -40,7 +39,7 @@ int i2c_read_register(int fd, uint8_t reg, uint8_t *data, size_t len) {
     return 0;
 }
 
-int i2c_write_register(int fd, uint8_t reg, const uint8_t *data, size_t len) {
+int i2c_write_register(int *fd, uint8_t reg, const uint8_t *data, size_t len) {
     uint8_t *buffer = (uint8_t *)malloc(len + 1);
     if (!buffer) {
         perror("Memory allocation failed");
@@ -50,7 +49,7 @@ int i2c_write_register(int fd, uint8_t reg, const uint8_t *data, size_t len) {
     buffer[0] = reg;
     memcpy(buffer + 1, data, len);
 
-    if (write(fd, buffer, len + 1) != (len + 1)) {
+    if (write(*fd, buffer, len + 1) != (len + 1)) {
         perror("Failed to write to register");
         free(buffer);
         return -1;
@@ -60,8 +59,8 @@ int i2c_write_register(int fd, uint8_t reg, const uint8_t *data, size_t len) {
     return 0;
 }
 
-int i2c_smbus_write(int fd, uint8_t reg, uint8_t value) {
-    if (i2c_smbus_write_byte_data(fd, reg, value) < 0) {
+int i2c_smbus_write(int *fd, uint8_t reg, uint8_t value) {
+    if (i2c_smbus_write_byte_data(*fd, reg, value) < 0) {
         perror("Failed to write byte data to register");
         return -1;
     }
@@ -69,8 +68,8 @@ int i2c_smbus_write(int fd, uint8_t reg, uint8_t value) {
     return 0;
 }
 
-int i2c_smbus_read(int fd, uint8_t reg, uint8_t *value) {
-    int result = i2c_smbus_read_byte_data(fd, reg);
+int i2c_smbus_read(int *fd, uint8_t reg, uint8_t *value) {
+    int result = i2c_smbus_read_byte_data(*fd, reg);
     if (result < 0) {
         perror("Failed to read byte data from register");
         return -1;
@@ -80,6 +79,6 @@ int i2c_smbus_read(int fd, uint8_t reg, uint8_t *value) {
     return 0;
 }
 
-void i2c_close(int fd) {
-    close(fd);
+void i2c_close(int *fd) {
+    close(*fd);
 }
---
+++ mpu6050/src/mpu6050.cpp
@@ -0,0 +1,183 @@
+#include "mpu6050.hpp"
+
+extern "C" {
+#include <errno.h>
+#include <fcntl.h>
+#include <i2c/smbus.h>
+#include <linux/i2c-dev.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+}
+
+MPU6050::MPU6050(int bus_number){
+    filename[9] = *std::to_string(bus_number).c_str();
+    if(i2c_init(filename, MPU6050_ADDR) < 0){
+        exit(1);
+    }
+    if(i2c_smbus_write(fd, PWR_MGMT_1, 0) < 0){
+        exit(1);
+    }
+    readGyroscopeRange();
+    readAccelerometerRange();
+    readDlpfConfig();
+}
+
+MPU6050::~MPU6050(){
+    i2c_close(fd);
+}
+
+void MPU6050::printConfig() const{
+    std::cout << "Accelerometer Range: +-" << ranges[ACC_R] << "g\n";
+    std::cout << "Gyroscope Range: +-" << ranges[GYR_R] << " degree per sec\n";
+    std::cout << "DLPF Range: " << ranges[DLPF_R] << " Hz\n";
+}
+
+void MPU6050::printOffsets() const{
+    std::cout << "Accelerometer Offsets: x: " << acc_offset[X] << ", y: " << acc_offset[Y]
+            << ", z: " << acc_offset[Z] << "\n";
+    std::cout << "Gyroscope Offsets: x: " << gyro_offset[X] << ", y: " << gyro_offset[Y]
+            << ", z: " << gyro_offset[Z] << "\n";
+}
+
+int MPU6050::readGyroscopeRange(){
+    int result;
+    if(i2c_smbus_read(fd, GYRO_CONFIG_REG, &result) < 0){
+        exit(1);
+    }
+    result = result >> 3;
+    ranges[GYR_R] = gyro_ranges[result];
+    return ranges[GYR_R];
+}
+
+int MPU6050::readAccelerometerRange(){
+    int result;
+    if(i2c_smbus_read(fd, ACC_CONFIG_REG, &result) < 0){
+        exit(1);
+    }
+    result = result >> 3;
+    ranges[ACC_R] = accel_ranges[result];
+    return ranges[ACC_R];
+}
+
+int MPU6050::readDlpfConfig(){
+    int result;
+    if(i2c_smbus_read(fd, DLPF_CONFIG_REG, &result) < 0){
+        exit(1);
+    }
+    result = result >> 3;
+    ranges[DLPF_R] = dlpf_ranges[result];
+    return ranges[DLPF_R];
+}
+
+void MPU6050::setGyroscopeRange(GyroRange range){
+    int result = i2c_smbus_write_byte_data(fd, GYRO_CONFIG_REG, range << 3);
+    if (result < 0) reportError(errno);
+    ranges[GYR_R] = gyro_ranges[static_cast<size_t>(range)];
+}
+
+void MPU6050::setAccelerometerRange(AccelRange range){
+    int result = i2c_smbus_write_byte_data(fd, ACC_CONFIG_REG, range << 3);
+    if (result < 0) reportError(errno);
+    ranges[ACC_R] = accel_ranges[static_cast<size_t>(range)];
+}
+
+void MPU6050::setDlpfBandwidth(DlpfBandwidth bandwidth){
+    int result = i2c_smbus_write_byte_data(fd, DLPF_CONFIG_REG, bandwidth);
+    if (result < 0) reportError(errno);
+    ranges[DLPF_R] = dlpf_ranges[static_cast<size_t>(bandwidth)];
+}
+
+double MPU6050::getAccelerationX() const
+{
+  int16_t acc_x_h = i2c_smbus_read_byte_data(fd, ACC_X_H);
+  int16_t acc_x_l = i2c_smbus_read_byte_data(fd, ACC_X_H + 1);
+  int16_t acc_x = acc_x_l | acc_x_h << 8;
+  double accel_x_converted = convertRawAccData(accel_x);
+  if (calibrated) {
+    return accel_x_converted - acc_offset[X];
+  }
+  return accel_x_converted;
+}
+
+double MPU6050::getAccelerationY() const
+{
+  int16_t accel_y_msb = i2c_smbus_read_byte_data(fd, ACC_Y_H);
+  int16_t accel_y_lsb = i2c_smbus_read_byte_data(fd, ACC_Y_H + 1);
+  int16_t accel_y = accel_y_lsb | accel_y_msb << 8;
+  double accel_y_converted = convertRawAccData(accel_y);
+  if (calibrated) {
+    return accel_y_converted - acc_offset[Y];
+  }
+  return accel_y_converted;
+}
+
+double MPU6050::getAccelerationZ() const
+{
+  int16_t accel_z_msb = i2c_smbus_read_byte_data(fd, ACC_Z_H);
+  int16_t accel_z_lsb = i2c_smbus_read_byte_data(fd, ACC_Z_H + 1);
+  int16_t accel_z = accel_z_lsb | accel_z_msb << 8;
+  double accel_z_converted = convertRawAccData(accel_z);
+  if (calibrated) {
+    return accel_z_converted - acc_offset[Z];
+  }
+  return accel_z_converted;
+}
+
+double MPU6050::getAngularVelocityX() const
+{
+  int16_t gyro_x_msb = i2c_smbus_read_byte_data(fd, GYRO_X_H);
+  int16_t gyro_x_lsb = i2c_smbus_read_byte_data(fd, GYRO_X_H + 1);
+  int16_t gyro_x = gyro_x_lsb | gyro_x_msb << 8;
+  double gyro_x_converted = convertRawGyroData(gyro_x);
+  if (calibrated) {
+    return gyro_x_converted - gyro_offset[X];
+  }
+  return gyro_x_converted;
+}
+
+double MPU6050::getAngularVelocityY() const
+{
+  int16_t gyro_y_msb = i2c_smbus_read_byte_data(fd, GYRO_Y_H);
+  int16_t gyro_y_lsb = i2c_smbus_read_byte_data(fd, GYRO_Y_H + 1);
+  int16_t gyro_y = gyro_y_lsb | gyro_y_msb << 8;
+  double gyro_y_converted = convertRawGyroData(gyro_y);
+  if (calibrated) {
+    return gyro_y_converted - gyro_offset[Y];
+  }
+  return gyro_y_converted;
+}
+
+double MPU6050::getAngularVelocityZ() const
+{
+  int16_t gyro_z_msb = i2c_smbus_read_byte_data(fd, GYRO_Z_H);
+  int16_t gyro_z_lsb = i2c_smbus_read_byte_data(fd, GYRO_Z_H + 1);
+  int16_t gyro_z = gyro_z_lsb | gyro_z_msb << 8;
+  double gyro_z_converted = convertRawGyroData(gyro_z);
+  if (calibrated) {
+    return gyro_z_converted - gyro_offset[Z];
+  }
+  return gyro_z_converted;
+}
+
+double MPU6050::convertRawAccData(int16_t gyro_raw) const{
+    const double ang_vel = static_cast<double>(gyro_raw) / gyro_map.at(ranges[GYR_R]);
+    return ang_vel; //angular velocity (deg/s)
+}
+
+double MPU6050::convertRawGyroData(int16_t accel_raw) const{
+    const double accel = static_cast<double>(accel_raw) / accel_map.at(ranges[ACC_R]) * GRAVITY;
+    return accel; // (m/s^2)
+}
+
+void MPU6050::setGyroOffset(double* offset){
+  gyro_offset[X] = offset[X];
+  gyro_offset[Y] = offset[Y];
+  gyro_offset[Z] = offset[Z];
+}
+
+void MPU6050::setAccOffset(double* offset){
+  acc_offset[X] = offset[X];
+  acc_offset[Y] = offset[Y];
+  acc_offset[Z] = offset[Z];
+}
