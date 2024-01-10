4524368f25f092879c3f84df61ecda13faffb181
serkan <serkanmazlum306@gmail.com>
2024-01-10 22:36:28 +0300

    The amount of data has been increased

--- test/mpu6050_sensor.cpp
+++ test/mpu6050_sensor.cpp
@@ -6,8 +6,8 @@ extern "C"{
 int main(){
     MPU6050 mpu6050;
     // mpu6050.calibrate();
-    std::cout << "\n\n\n\n";
-    for(int i = 0; i < 100; i++){
+    std::cout << "\n\n\n\n\n\n\n\n\n";
+    for(int i = 0; i < 10000; i++){
         mpu6050.cleanTerminal();
         mpu6050.printAcceleration();
         mpu6050.printAngularVelocity();
