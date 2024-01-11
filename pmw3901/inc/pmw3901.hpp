fdf8d154266499965971002874cced982ff05170
serkan <serkanmazlum306@gmail.com>
2024-01-11 13:41:36 +0300

    incomplete pmw code

---
+++ pmw3901/inc/pmw3901.hpp
@@ -0,0 +1,20 @@
+#ifndef __PMW3901_HPP__
+#define __PMW3901_HPP__
+extern "C"{
+#include <stdint.h>
+// #include <std
+
+}
+
+// class PMW3901{
+// public:
+//     PMW3901();
+//     ~PMW3901();
+//     void readData();
+
+// private:
+//     int x;
+//     int y;
+// };
+
+#endif
