6fe389b4d2f526030f8d15dabec1180ba3ec4479
serkan <serkanmazlum306@gmail.com>
2024-01-21 13:11:30 +0300

    bug fix

--- test/lcd_display.cpp
+++ test/lcd_display.cpp
@@ -1,12 +1,13 @@
 #include "lcd_2x16.hpp"
 
-#define DELAY 10000
+#define DELAY 100000
 
 int main(){
     LCD lcd;
     char number[12];
     for(int i = 0; i < 100; i++){
         lcd.clear();
+        lcd.setCursor(0, 1);
         snprintf(number, 12, "Number: %d", i);
         lcd.writeString(number);
         usleep(DELAY);
