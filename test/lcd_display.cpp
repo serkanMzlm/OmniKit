c9a0741cf7827f302abacef0ce51062c408c4ca3
serkan <serkanmazlum306@gmail.com>
2024-01-21 13:06:49 +0300

    bug fix

--- test/lcd_display.cpp
+++ test/lcd_display.cpp
@@ -2,10 +2,10 @@
 
 int main(){
     LCD lcd;
-    char* number;
+    char number[12];
     for(int i = 0; i < 100; i++){
         lcd.clear();
-        snprintf(number, 10, "Number: %d", i);
+        snprintf(number, 12, "Number: %d", i);
         lcd.writeString(number);
     }
     getchar();
