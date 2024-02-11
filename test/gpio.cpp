05a3ed28c2a91ab27cc638c657d8a609dc1e7c8d
serkan <serkanmazlum306@gmail.com>
2024-02-11 17:35:54 +0300

    terminal values assigned to variable

--- test/gpio.cpp
+++ test/gpio.cpp
@@ -9,8 +9,8 @@ int main(int argc, char** argv){
     std::cout << "Toplam argüman sayısı: " << argc << std::endl;
     std::cout << "Program adı: " << argv[0] << std::endl;
     if (argc > 2) {
-        int pin = atoi(argv[1].c_str());
-        int value = atoi(argv[2].c_str());
+        int pin = atoi(argv[1]);
+        int value = atoi(argv[2]);
 
         unexportPin(pin);
         usleep(DELAY);
