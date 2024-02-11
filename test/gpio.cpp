9563205f74f6503e17ca57a1bac918e5bb3ceb90
serkan <serkanmazlum306@gmail.com>
2024-02-11 20:19:34 +0300

    name edited

--- test/gpio.cpp
+++ test/gpio.cpp
@@ -6,8 +6,8 @@
 #define DELAY 100000
 
 int main(int argc, char** argv){
-    std::cout << "Toplam argüman sayısı: " << argc << std::endl;
-    std::cout << "Program adı: " << argv[0] << std::endl;
+    std::cout << "total number of arguments: " << argc << std::endl;
+    std::cout << "program name: " << argv[0] << std::endl;
     if (argc > 2) {
         std::cout << "pin: " << argv[1] << " value: " << argv[2] << std::endl;
         int pin = atoi(argv[1]);
