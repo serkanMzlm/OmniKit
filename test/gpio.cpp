621f2fae8049f58fa2a58e9dc9cc1b88a4002372
serkan <serkanmazlum306@gmail.com>
2024-02-11 12:58:20 +0300

    added file closing

--- test/gpio.cpp
+++ test/gpio.cpp
@@ -7,7 +7,8 @@ int main(int argc, char** argv){
     std::cout << "Toplam argüman sayısı: " << argc << std::endl;
     std::cout << "Program adı: " << argv[0] << std::endl;
     if (argc > 2) {
-        exportPin(std::stoi(argv[1]));
+        unexportPin(17);
+        exportPin(17);
         directionPin(17, OUTPUT);
         writePin(17, 0);
     }
