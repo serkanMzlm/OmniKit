91df8bfadfb28b538405dbc9034a087c4a2a6ceb
serkan <serkanmazlum306@gmail.com>
2024-02-11 17:29:46 +0300

    string to int

--- test/gpio.cpp
+++ test/gpio.cpp
@@ -8,13 +8,13 @@ int main(int argc, char** argv){
     std::cout << "Toplam argüman sayısı: " << argc << std::endl;
     std::cout << "Program adı: " << argv[0] << std::endl;
     if (argc > 2) {
-        unexportPin(std::stio(argv[1]));
+        unexportPin(std::stoi(argv[1]));
         usleep(DELAY);
-        exportPin(std::stio(argv[1]));
+        exportPin(std::stoi(argv[1]));
         usleep(DELAY);
-        directionPin(std::stio(argv[1]), OUTPUT);
+        directionPin(std::stoi(argv[1]), OUTPUT);
         usleep(DELAY);
-        writePin(std::stio(argv[1]), std::stio(argv[0]));
+        writePin(std::stoi(argv[1]), std::stoi(argv[0]));
     }
 
     return 0;
